0accf7c21749adb60966463b0107e846c9277cd6
JasperBoutte <155947817+JasperBoutte@users.noreply.github.com>
2024-03-06 02:39:26 -0800

    Add files via upload

---
+++ site/script.js
@@ -0,0 +1,83 @@
+// Define an array of objects representing team members with their info.
+const teamMembers = [
+    {
+        name: "Olena",
+        role: "Team Role- Analyst     |     Links:https://www.facebook.com/OlenaTargetYelizarova   -   https://www.linkedin.com/feed/   -   https://profile.indeed.com/?hl=en_US&co=US&from=gnav-notifcenter",
+        placeholderimg: "https://media.discordapp.net/attachments/1207030108884566037/1214866480043397160/SmallOlena.png?ex=65faab93&is=65e83693&hm=fcfe599cf98b5c9c93fd575a58f18d84543cb646a36bd93877a5ab1525d71fe7&=&format=webp&quality=lossless",
+        image: "url('https://media.discordapp.net/attachments/1207030108884566037/1212955995102248960/Olenas_Headshot.jpg?ex=65f3b84c&is=65e1434c&hm=0fbd7d5a893a5d5595d87f3346ef03103ed6a8ffb227152064d229e9d3f7a02b&=&format=webp&width=541&height=676')",
+        info: `Education:
+  Currently pursuing Early Childhood Education at North Seattle College.
+  I have a Bachelor's degree in Social Pedagogics University, in Ukraine, and also completed studies in business and commerce College of Trade and Commerce, and culinary arts at Culinary College.
+  
+  Work Experience:
+  Now I am employed as a teacher's assistant at the North Seattle College Child Care Center, providing support and guidance to young children. Additionally, serves as a supervisor at Papa Murphy's Pizza, overseeing daily operations and ensuring customer satisfaction. Previous roles include head of sales department at Uniqa Insurance Company, Kyiv, Ukraine, and forest family program trainer at Kyiv Center for Social Services. Also worked as a Chef at Fairmont Grand Hotel, international cruise services, and Marriott hotels.
+  My hobbies revolve around studying and traveling, having explored over 50 countries. I prioritize supporting Ukraine, raising my son, and planning to adopt another child. I intend to return to Ukraine to aid children affected by Russia's unprovoked invasion.`
+      },
+      {
+        name: "Evan",
+        role: "Team Role- Project Manager |          Links:https://www.linkedin.com/in/evan-wallace-816561230/   -   https://github.com/RenascentBard98",
+        placeholderimg: "https://media.discordapp.net/attachments/1207030108884566037/1214863626737811488/Smallevean.png?ex=65faa8eb&is=65e833eb&hm=8b43f4babcd694fb117f5c08c4582bbcc13605593ab1ac4354a9bd82575976ef&=&format=webp&quality=lossless",
+        image: "url('https://media.discordapp.net/attachments/1207030108884566037/1214697314594062346/Evans_Headshot.png?ex=65fa0e07&is=65e79907&hm=6f1c12863f8e18c7c1275502d9ebe715bf65d176ad71dda56b4da5ccf01821a1&=&format=webp&quality=lossless&width=350&height=350')",
+        info: "Despite my love for the great indoors, I still would consider myself a PNW native through and through. You can catch me cooking fresh salmon on a wood plank, coding in coffee shops, playing video games, and enjoying cloudy Seattle weather that makes you wonder if you’ll ever be happy in your entire life. I was born and raised in Portland, Oregon. I spent most of my time there on weekly field trips facilitated by Sunnyside Environmental where I attended K-8, taking road trips to the Oregon coast, and being a major cause of “out of order” signs on exhibits at OMSI (Oregon Museum of Science and Industry). Since the age of 13, Seattle has been my home, and its vibrant culture and diverse community have greatly influenced my life. Directly after high school, I began my professional journey at PCC Community Markets, where I immersed myself in various roles, ranging from cashiering to assuming the responsibilities of a grocery team lead. These experiences not only honed my skills in customer service and leadership but also instilled in me a deep appreciation for the importance of community and sustainable practices in the workplace. I embarked on my academic journey at North Seattle College in the fall of 2022, where I was first introduced to the captivating world of computer programming. It was there that I unearthed a passion for the intricacies of coding and software development. Motivated by this newfound interest, I am currently dedicated to earning a Bachelor’s of Applied Science in Application Development, aiming to expand my knowledge and skills in this dynamic field."
+      },
+      {
+        name: "Justin",
+        role: "Team Role- Analyst  |        Links:https://www.linkedin.com/in/justin-phounsavan-45a01b93/   -   https://github.com/goodstoryAI",
+        placeholderimg: "https://media.discordapp.net/attachments/1207030108884566037/1207030298047553556/justin.png?ex=65f9d910&is=65e76410&hm=ea1b5cb2f0fdc50a0cd4926e828aca53372579498b8ad23f05b75e7a16b60188&=&format=webp&quality=lossless",
+        image: "url('https://media.discordapp.net/attachments/1207030108884566037/1207030298047553556/justin.png?ex=65f9d910&is=65e76410&hm=ea1b5cb2f0fdc50a0cd4926e828aca53372579498b8ad23f05b75e7a16b60188&=&format=webp&quality=lossless')",
+        info: "Golf has become my all-consuming passion, driving me to dedicate countless hours and resources to mastering the game. Yet, my love for the outdoors extends beyond the fairways, encompassing activities like paddleboarding, backpacking, snowboarding, and more. These adventures have led me to significant milestones, from completing the grueling Seattle to Portland bike ride to conquering a half marathon, with more challenges on the horizon. In addition to my outdoor pursuits, I'm deeply passionate about graphic design, particularly for web-based content. Whether I'm crafting captivating visuals or editing dynamic videos, I find immense joy in the creative process. With over a decade of experience in the automotive digital space, I'm eager to merge my passion for design with industry expertise, creating engaging content that enhances the digital presence of automotive clients."
+      },
+      {
+        name: "Jasper",
+        role: "Team Role- Dev    |      Links:https://www.linkedin.com/in/jasper-boutte-0b5a94210/   -   https://github.com/JasperBoutte",
+        placeholderimg: "https://media.discordapp.net/attachments/1207030108884566037/1214864140988973136/Jasperpfpsmall.png?ex=65faa966&is=65e83466&hm=4a6bb3a4f1955816075ec565263ccd76e2d2df1312af7264696aee9dbe608448&=&format=webp&quality=lossless",
+        image: "url('https://media.discordapp.net/attachments/1207030108884566037/1212222993925939260/IMG_2441.JPG?ex=65f10da3&is=65de98a3&hm=71d05f85d3fdbe7a4d3f77a0985a7e43daccd0a81bc76619373a87e7995d3ee1&=&format=webp&width=676&height=676')",
+        info: "My name is Jasper, and I am 18 years old from Seattle, Washington. I attended TOPS K-8, a place where I learned to discern truth from lies and developed some of my worst habits, yet still remember it as a tremendously fun time. I graduated from TOPS during the first year of COVID, completing my freshman year of high school online. During quarantine, I rediscovered my love for computers. After building two, I found satisfaction in working on something that I would use in my life. This period also allowed me to spend a lot of time online, talking to strangers about coding, video games, and other interests. I developed a passion for various subjects, but most importantly, it spurred my continued interest in all things digital. I found my community mainly on Discord servers. Here, I saw how my interests could be used for fun in both good and bad ways. Ultimately, deciding against becoming a cybercriminal steered me towards the path I am on today. The three-plus years I spent online learning valuable skills and a new perspective that pushed my limits. Throughout high school, I experimented with new technologies, realizing quite late how much more I could achieve. I decided to explore every avenue possible, making as many friends as I could and learning from each of them. I focused particularly on cybersecurity and penetration testing. I acquired toys from Hak5, among other tools, experimenting with them and seeing what they could do. Eventually, when OpenAI finally released their first publicly accessible model, I discovered my new favorite thing: AI. AI is the most fun double-edged sword I've played with; it gave me the ability to do things I would not have been able to do otherwise. This gave me the urge to go deeper into understanding how LLMs and AI work and how to create them. In the summer of my junior year, I had the incredible opportunity to intern as a full-stack developer at T-Mobile. This experience gave me insight into the workings of a professional environment and fueled my interest in AI and other technologies further."
+      },
+      {
+        name: "Nima",
+        role: "Team Role- Analyst    |      Link:https://profile.indeed.com/?hl=en_US&co=US&from=gnav-homepage",
+        placeholderimg: "https://media.discordapp.net/attachments/1207030108884566037/1212955793960206398/newNima.png?ex=65f3b81c&is=65e1431c&hm=fa3d33ed1973c721cc9848980e0c56a738c4b9e293246b0cefccb225faffa094&=&format=webp&quality=lossless",
+        image: "url('https://media.discordapp.net/attachments/1207030108884566037/1212955793960206398/newNima.png?ex=65f3b81c&is=65e1431c&hm=fa3d33ed1973c721cc9848980e0c56a738c4b9e293246b0cefccb225faffa094&=&format=webp&quality=lossless')",
+        info: `Love of freedom, establishing democracy, and human rights became my passion when I was a teenager, but at the same time, an equal interest and curiosity in engineering and technology were injected into my blood. Despite enjoying being in nature and hiking a beautiful mountain, I still like to sit down behind my computer, researching, and learning new things. 
+  
+  I was born and raised in Iran-Tehran, a very beautiful country with a very rich history and culture. When I say a rich history, that means that part of the earth has influenced human civilization since its existence from seven thousand years ago. My land of origin has had a lot of heroic moments, moments where its existence was on the edge of collapse but it stood up again and was reborn like a phoenix which burnt out to ash and the new bird and life born from its ash. My beloved land is in another dark moment of its history; it will get another new birth very soon.
+  
+  Seattle and the US have become my second home since 2010. I like Seattle's weather, but the cloudy sky of this green vibrant city sometimes makes me more active indoors. I started to work in the kitchen and did that job from scratch as a dishwasher and prep cook till lead cook and line cook. I worked for some good names in this industry and the city of Seattle, such as Whole Foods Market, Metropolitan Market, PCC Community Market, and recently Sheraton Grand Seattle. Working in this industry helped me learn and gain a lot of skills in teamwork, customer service, creativity and creating menu items, and also management. 
+  
+  When I got to Seattle, studying and finishing my degree in computer engineering became my goal. I started my journey at North Seattle College as an ESL student. I had to improve my English skills, despite that language skill is an open ending as computer coding language. I started to enroll in academic courses from 2014 and have been through half of my degree; funding for college expenses and the pandemic were most causes of delay in the accomplishment of my academic goal.
+  
+  Right now, more positive than ever, I would like to finish my study and move on to another stage of my life.`
+      },
+      ];
+
+
+      function createProfilePictures() {
+        const container = document.getElementById('team-members');
+        teamMembers.forEach((member, index) => {
+          const profileDiv = document.createElement('div');
+          profileDiv.className = 'profile-pic';
+          profileDiv.style.backgroundImage = member.image;
+          // I hate this
+          profileDiv.style.top = `${70 - index * 12.5}%`;
+          profileDiv.style.left = `${10 + (index % 2) * 10}%`;
+          profileDiv.addEventListener('click', () => openModal(member));
+          container.appendChild(profileDiv);
+        });
+      }
+
+
+      function openModal(member) {
+        const modal = document.getElementById('modal');
+        document.getElementById('modal-img').src = member.placeholderimg;
+        document.getElementById('modal-name').textContent = member.name;
+        document.getElementById('modal-role').textContent = member.role;
+        document.getElementById('modal-info').textContent = member.info;
+        modal.style.display = 'block';
+      }
+      
+      document.querySelector('.close').addEventListener('click', () => {
+        document.getElementById('modal').style.display = 'none';
+      });
+      
+      document.addEventListener('DOMContentLoaded', createProfilePictures);
